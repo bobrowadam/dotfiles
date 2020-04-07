b3172dc5a594fa0f18f6767453ca2d3a0331310d
Adam Bobrow <bob@Adams-MacBook-Pro.local>
2020-04-08 02:18:41 +0300

    Add more brew pakcages

--- emacs-installer.sh
+++ emacs-installer.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
-brew install emacs-plus --with-modern-icon;
+brew tap d12frosted/emacs-plus;
+brew install emacs-plus --HEAD --with-jansson --with-modern-icon;
 brew install gpg2;
 brew install bat;
+brew install gawk;
+brew install http;
