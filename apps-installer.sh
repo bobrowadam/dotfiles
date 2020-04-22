90efc3f6078b0f2ca80ed6542fe31980c328508f
Adam Bobrtow <bob@Adams-MacBook-Pro-2.local>
2020-04-22 12:48:38 +0300

    Install zsh

--- apps-installer.sh
+++ apps-installer.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+mkdir ~/source;
+touch ~/secrets.sh
 
 brew tap d12frosted/emacs-plus;
 brew install emacs-plus --HEAD --with-jansson --with-modern-icon;
@@ -6,6 +8,10 @@ brew install gpg2;
 brew install bat;
 brew install gawk;
 brew install http;
+brew install zsh;
+chsh -s /bin/zsh;
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
 # rust:
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh;
 rustup component add rls --toolchain stable-x86_64-apple-darwin;
